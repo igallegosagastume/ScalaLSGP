4285e4e1830305b6dbd5cee113320d52cd24a507
Ignacio Gallego Sagastume <bluemontag@gmail.com>
2018-08-08 23:46:38 -0300

    Incomplete algorithm with bugs, but closer

--- src/seqgen/model/SeqGenWithReplacementMap.scala
+++ src/seqgen/model/SeqGenWithReplacementMap.scala
@@ -9,44 +9,70 @@ class SeqGenWithReplacementMap(order:Int) {
   val allNulls : AbstractLatinSquare[Int] = VectorLatinSquare.getFillableLS(order)
   
   def generateRow(i:Int, partialLatinSquare:AbstractLatinSquare[Int]) : AbstractLatinSquare[Int] = {
+    val emptyRow = Vector()
     
-    val row = Vector.tabulate(partialLatinSquare.order)(j => generateElem(i, j, partialLatinSquare) )
+    val row = this.generateRowWithPartialRow(i, 0, partialLatinSquare, emptyRow)//Vector.tabulate(partialLatinSquare.order)(j => generateElem(i, j, partialLatinSquare) )
     
     partialLatinSquare.setRow(i, row)
   }
-  
-  protected def generateElem(i:Int, j:Int, partialLS:AbstractLatinSquare[Int]) : Int = {
-    val emptyRow = Vector()
-    val elem = this.generateElem(i, j, partialLS, emptyRow)
-    
-    elem
+
+  protected def generateRowWithPartialRow(i: Int, j: Int, partialLS: AbstractLatinSquare[Int], partialRow: Vector[Int]): Vector[Int] = {
+
+    if (partialRow.size == partialLS.order) {
+      partialRow
+    } else {
+      val elem = this.generateElem(i, j, partialLS, partialRow)
+
+      if (!partialRow.contains(elem)) {
+        this.generateRowWithPartialRow(i, j + 1, partialLS, partialRow :+ elem)
+      } else {
+        val newPartialRow = this.makeSpaceForElem(elem, i, j, partialLS, partialRow)
+        //val elem2 = this.generateElem(i, j, partialLS, newPartialRow)
+        //now elem2 fits in position (i,j)
+        this.generateRowWithPartialRow(i, j + 1, partialLS, partialRow :+ elem)
+      }
+    }
   }
   
-  private def generateElem(i:Int, j:Int, partialLS:AbstractLatinSquare[Int], partialRow:Vector[Int]) : Int = {
+  
+  private def makeSpaceForElem(elem:Int, i:Int, j:Int, partialLS:AbstractLatinSquare[Int], partialRow:Vector[Int]) : Vector[Int] = {
     
-    val availInRow = partialLS.availInRow(i)
+    /*val replacementMap = this.generateReplacementMap(partialLS)
+    
+    for (i <- replacementMap) {
+      println(i)
+    }*/
+    
+    val pos = partialRow.indexOf(elem)
+    
+    if (pos == -1) {
+      partialRow
+    } else {
+      val possibleInColumnPos = partialLS.availInCol(pos)
+      val actualElem = Vector(partialRow(pos))
+      val newElem = RandomUtils.randomChoice(possibleInColumnPos.diff(actualElem))
+      makeSpaceForElem(newElem, i, j, partialLS, partialRow.updated(pos, newElem))
+    }
+  }
+  
+  private def generateElem(i: Int, j: Int, partialLS: AbstractLatinSquare[Int], partialRow: Vector[Int]): Int = {
+    val availInRow = partialLS.allNumsVector.diff(partialRow)
     val availInCol = partialLS.availInCol(j)
-    val availInPos = availInRow.diff(availInCol)
-    val newPartialRow = if (availInPos.isEmpty) {//collision
-                          this.makeSpace(i, j, partialLS, partialRow)
-                        } else {
-                          partialRow
-                        }
-    
-    val elem = RandomUtils.randomChoice(availInPos)
-    
-    elem
+    val availInPos = availInRow.intersect(availInCol)
+
+    if (availInPos.isEmpty) {
+      RandomUtils.randomChoice(availInCol)
+    } else {
+      RandomUtils.randomChoice(availInPos)
+    }
   }
   
-  private def makeSpace(i:Int, j:Int, partialLS:AbstractLatinSquare[Int], partialRow:Vector[Int]) : Vector[Int] = {
-    partialRow
-  }
   /**
    * PRECONDITION: The LS has been filled up to the (row-1)-th row (inclusive).
    * 
    *  The result at i will contain the available elements in column i
    */
-  def generateReplacementMap(row:Int, partialLatinSquare: AbstractLatinSquare[Int]) : Vector[Vector[Int]] = {
+  def generateReplacementMap(partialLatinSquare: AbstractLatinSquare[Int]) : Vector[Vector[Int]] = {
     
     Vector.tabulate(partialLatinSquare.order)(col => partialLatinSquare.availInCol(col))
     
@@ -57,18 +83,26 @@ class SeqGenWithReplacementMap(order:Int) {
 object SeqGenWithReplacementMap {
   
   def main(args:Array[String]) = {
+    val generator = new SeqGenWithReplacementMap(5)
     val ls = VectorLatinSquare.getFillableLS(5)
-    
     
     val ls2 = ls.setRow(0, Vector.tabulate(5)(i => i))
     
-//    print(ls2)
+    val ls3 = generator.generateRow(1, ls2)
     
-    val generator = new SeqGenWithReplacementMap(5)
+    val ls4 = generator.generateRow(2, ls3)
     
-//    println(generator.generateReplacementMap(0, ls2))
+    val ls5 = generator.generateRow(3, ls4)
     
-    println(generator.generateRow(1, ls2))
+    val ls6 = generator.generateRow(4, ls5)
+    
+    println(ls6)
+    
+//    val ls11 = ls.setRow(0, Vector(0,1,2,3,4))
+//    val ls22 = ls11.setRow(1, Vector(1,3,0,2, RandomUtils.getNullElem()))
+//    
+//    println(ls22)
+//    print(generator.makeSpaceForElem(4, 1, 4, ls22, Vector(1,3,0,2)))
     
   }
 }
